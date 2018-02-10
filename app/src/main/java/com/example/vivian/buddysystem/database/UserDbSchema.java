97e3d96ad4bee1c533aa238cbda1a3c3d6f66baa
Charlotte Wright - Spectre <RandomWright@users.noreply.github.com>
2018-02-10 14:57:34 -0500

    database for user

--- app/src/main/java/com/example/vivian/buddysystem/database/UserDbSchema.java
+++ app/src/main/java/com/example/vivian/buddysystem/database/UserDbSchema.java
@@ -13,6 +13,10 @@ public class UserDbSchema {
 
         public static final class Cols {
             public static final String ID = "id";
+            public static final String USERNAME = "username";
+            public static final String NAME = "name";
+            public static final String PHONE = "phone";
+            public static final String PASSWORD = "password";
 
         }
     }
---
+++ app/src/main/java/com/example/vivian/buddysystem/model/User.java
@@ -0,0 +1,19 @@
+package com.example.vivian.buddysystem.model;
+
+import java.util.UUID;
+
+/**
+ * Created by Charlotte on 2/10/2018.
+ */
+
+public class User {
+    private static final String TAG = "USER";
+
+    private UUID id;
+    private String username;
+    private String name;
+    private String photo;
+    private String password;
+
+}
+
