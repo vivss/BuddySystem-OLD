347391a118f6d59c3b118428a5b2801ced7637b6
Charlotte Wright - Spectre <RandomWright@users.noreply.github.com>
2018-02-10 15:26:51 -0500

    link me bbaby

--- app/src/main/java/com/example/vivian/buddysystem/model/User.java
+++ app/src/main/java/com/example/vivian/buddysystem/model/User.java
@@ -15,5 +15,36 @@ public class User {
     private String photo;
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
