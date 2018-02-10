1c176fd406cdf1a7122848bec869804330b8f814
Charlotte Wright - Spectre <RandomWright@users.noreply.github.com>
2018-02-10 14:25:06 -0500

    creating a database

---
+++ app/src/main/java/com/example/vivian/buddysystem/database/UserDbSchema.java
@@ -0,0 +1,19 @@
+package com.example.vivian.buddysystem.database;
+
+/**
+ * Created by Charlotte on 2/10/2018.
+ * Schema for the User Database
+ */
+
+public class UserDbSchema {
+    public static final String DATABASE_NAME = "users.db";
+
+    public static final class UserTable {
+        public static final String TABLENAME = "users";
+
+        public static final class Cols {
+            public static final String ID = "id";
+
+        }
+    }
+}
